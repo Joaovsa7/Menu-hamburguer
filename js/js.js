bece7a51325cf3de8bec2ee9e3779b26a4fd1c8d
joaovsa7 <jvsadesign@gmail.com>
2018-08-24 09:53:29 -0300

    verificar o codigo

--- js/js.js
+++ js/js.js
@@ -2,10 +2,9 @@ let botao = document.querySelector("#hamburger");
 
 let lateral = document.querySelector("aside");
 
-let barraVisivel = false;
 
 botao.onclick = function () {
-    
+
         lateral.classList.toggle('mostrando');
         for(item of lateral.children){
             if  (item.id !== "hamburguer"){
@@ -14,6 +13,9 @@ botao.onclick = function () {
             
     } 
 
+        for(item of botao.children) {
+            item.classlist.toggle('mostrando');
+        }
 
 }
    
